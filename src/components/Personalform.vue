7cfd1ca3b83b1b55feed064d842e61df0b7d65de
Atohallan <apollyon0929@gmail.com>
2023-04-26 04:01:06 -0400

    feat: Becreftelse, Sammendrage, Smsverifisering

---
+++ src/components/Personalform.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-text-field label="Fult navn"></v-text-field>
+      <v-text-field label="Telefonnummer"></v-text-field>
+      <v-text-field label="E-post"></v-text-field>
+      <v-text-field label="Regnummer / chasisnr"></v-text-field>
+      <v-text-field label="KM-stand"></v-text-field>
+      <v-textarea label="Melding til verkstedet"></v-textarea>
+      <v-btn block rounded="xl" color="amber">Booking</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts" setup></script>
---
+++ src/components/Type.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-h6 font-weight-bold text-uppercase">types</h1>
+</template>
+
+<script lang="ts" setup></script>
